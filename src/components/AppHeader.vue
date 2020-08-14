3c5e44938c14e6ba32b1e0b28817a490a88cd15d
hah <ahrabi@denkformat.de>
2020-08-14 16:06:31 +0200

    new appheader created

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div class="logo">Denkformat</div>
+    <ul id="nav">
+      <router-link to="/" tag="li" exact> <a>Home</a> </router-link
+      >|
+      <router-link to="/about" tag="li"> <a>About</a> </router-link
+      >|
+      <router-link to="/help" tag="li" > <a>Help</a> </router-link
+      >|
+      <router-link to="/contact" tag="li">
+        <a>Contact</a>
+      </router-link>
+    </ul>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: "AppHeader"
+};
+</script>
